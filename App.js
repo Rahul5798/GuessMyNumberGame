90b9487b26fd8a1bb9975da01c145341e1d3ae3f
Rahul Patel <63693793+Rahul5798@users.noreply.github.com>
2023-10-28 12:37:48 -0400

    Created Custom Buttons for StartGameScreen

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from "react-native";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
+<<<<<<< HEAD
+  return <StartGameScreen />;
+=======
   return (
     <View style={styles.container}>
-      <Text>Hello World!!!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
+>>>>>>> parent of acaf2c1 (Setting up screen components)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
